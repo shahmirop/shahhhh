5909f2aca348091f918e9f17286bd93c36f1fd6f
shahmirop <101967489+shahmirop@users.noreply.github.com>
2022-03-20 13:00:40 +0500

    Add files via upload
    love

---
+++ main.dart
@@ -0,0 +1,50 @@
+void main() {
+  var ab =
+      "                      =======================                                    ";
+  print(ab);
+
+  var ab1 =
+      "                       STUDENT NAME : SHAHMIR                                ";
+  print(ab1);
+
+  var ab2 =
+      "                       ___________________________                                    ";
+  print(ab2);
+
+  var ab3 =
+      "                 [[[[[QUESTION NO 4]]]]]]                         ";
+  print(ab3);
+
+  var hww = ['apple', 'banana', 'mango', 'orange', 'strawberry'];
+  for (var x in hww) {
+    print(x);
+  }
+
+  var ab5 =
+      "                         --------------------                                    ";
+  print(ab5);
+
+  var ab4 = "                   [[[[QUESTION NO 5]]]]                          ";
+  print(ab4);
+  var num = 7;
+  for (var a = 2; a <= 15; a++) {
+    print("${num} x ${a} = ${num * a} ");
+  }
+
+  var ab6 = "                   ///QUESTION NO 1///                          ";
+  print(ab6);
+
+  var words = {'ahmed', 'ahmed', 'ahmed', 'haseeb', 'haseeb', 'haseeb'};
+  words.remove(1);
+  print(words);
+
+  var gg = "                  -----------------------                     ";
+  print(gg);
+
+  var ab7 = "                   ///QUESTION NO 11///                          ";
+  print(ab7);
+
+  var words3 = {'zahid', 'zahid', 'zahid', 'amir', 'amir', 'amir'};
+  words3.remove(1);
+  print(words);
+}
